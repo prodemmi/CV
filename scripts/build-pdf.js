171d27490cb068750a8066023587a84cc588b868
Emad Malekpour <prodemmi@gmail.com>
2023-06-16 16:55:56 +0330

    [FEATURE] Add multi language support

--- scripts/build-pdf.js
+++ scripts/build-pdf.js
@@ -5,72 +5,77 @@ var path = require('path');
 const puppeteer = require('puppeteer');
 require('dotenv').config();
 
+const LANGS = ["en", "fa"];
+
 (async () => {
-  const htmlFile = path.resolve(__dirname + '/../dist/index.html');
-  const filename = path.resolve(__dirname + '/../dist/assets/pdf/EmadMalekpour-SeniorDeveloper_EN.pdf');
-  fs.mkdirSync(path.dirname(filename), { recursive: true });
 
-  const options = {
-    headless: true,
-    args: ['--disable-features=IsolateOrigins',
-      '--disable-site-isolation-trials',
-      '--autoplay-policy=user-gesture-required',
-      '--disable-background-networking',
-      '--disable-background-timer-throttling',
-      '--disable-backgrounding-occluded-windows',
-      '--disable-breakpad',
-      '--disable-client-side-phishing-detection',
-      '--disable-component-update',
-      '--disable-default-apps',
-      '--disable-dev-shm-usage',
-      '--disable-domain-reliability',
-      '--disable-extensions',
-      '--disable-features=AudioServiceOutOfProcess',
-      '--disable-hang-monitor',
-      '--disable-ipc-flooding-protection',
-      '--disable-notifications',
-      '--disable-offer-store-unmasked-wallet-cards',
-      '--disable-popup-blocking',
-      '--disable-print-preview',
-      '--disable-prompt-on-repost',
-      '--disable-renderer-backgrounding',
-      '--disable-setuid-sandbox',
-      '--disable-speech-api',
-      '--disable-sync',
-      '--hide-scrollbars',
-      '--ignore-gpu-blacklist',
-      '--metrics-recording-only',
-      '--mute-audio',
-      '--no-default-browser-check',
-      '--no-first-run',
-      '--no-pings',
-      '--no-sandbox',
-      '--no-zygote',
-      '--password-store=basic',
-      '--use-gl=swiftshader',
-      '--use-mock-keychain']
+  for (const lang of LANGS) {
+    const htmlFile = path.resolve(__dirname + `/../dist/index-${lang}.html`);
+    const filename = path.resolve(__dirname + `/../dist/assets/pdf/EmadMalekpour-SeniorDeveloper-${lang}.pdf`);
+    fs.mkdirSync(path.dirname(filename), { recursive: true });
+
+    const options = {
+      headless: true,
+      args: ['--disable-features=IsolateOrigins',
+        '--disable-site-isolation-trials',
+        '--autoplay-policy=user-gesture-required',
+        '--disable-background-networking',
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-breakpad',
+        '--disable-client-side-phishing-detection',
+        '--disable-component-update',
+        '--disable-default-apps',
+        '--disable-dev-shm-usage',
+        '--disable-domain-reliability',
+        '--disable-extensions',
+        '--disable-features=AudioServiceOutOfProcess',
+        '--disable-hang-monitor',
+        '--disable-ipc-flooding-protection',
+        '--disable-notifications',
+        '--disable-offer-store-unmasked-wallet-cards',
+        '--disable-popup-blocking',
+        '--disable-print-preview',
+        '--disable-prompt-on-repost',
+        '--disable-renderer-backgrounding',
+        '--disable-setuid-sandbox',
+        '--disable-speech-api',
+        '--disable-sync',
+        '--hide-scrollbars',
+        '--ignore-gpu-blacklist',
+        '--metrics-recording-only',
+        '--mute-audio',
+        '--no-default-browser-check',
+        '--no-first-run',
+        '--no-pings',
+        '--no-sandbox',
+        '--no-zygote',
+        '--password-store=basic',
+        '--use-gl=swiftshader',
+        '--use-mock-keychain']
+    }
+
+    if (process.env.DEBUG === "true") {
+      options.executablePath = "/usr/bin/google-chrome";
+    }
+
+    const browser = await puppeteer.launch(options)
+
+    const page = await browser.newPage();
+
+    await page.goto(`file://${htmlFile}`, { waitUntil: 'networkidle0' });
+
+    await page.emulateMediaType('print');
+
+    await page.pdf({
+      path: filename,
+      printBackground: true,
+      format: 'tabloid',
+      scale: 0.74,
+      displayHeaderFooter: false,
+      landscape: true
+    });
+
+    await browser.close();
   }
-
-  if (process.env.DEBUG === "true"){
-    options.executablePath = "/usr/bin/google-chrome";
-  }
-
-  const browser = await puppeteer.launch(options)
-
-  const page = await browser.newPage();
-
-  await page.goto(`file://${htmlFile}`, { waitUntil: 'networkidle0' });
-
-  await page.emulateMediaType('print');
-
-  await page.pdf({
-    path: filename,
-    printBackground: true,
-    format: 'tabloid',
-    scale: 0.74,
-    displayHeaderFooter: false,
-    landscape: true
-  });
-
-  await browser.close();
 })()
--- scripts/render-pug.js
+++ scripts/render-pug.js
@@ -7,38 +7,43 @@ const prettier = require('prettier');
 var { get } = require('lodash');
 const translations = require("../src/assets/translations.json");
 
+const LANGS = ["en", "fa"];
+
 module.exports = function renderPug(filePath) {
-    const destPath = filePath.replace(/src\/pug\//, 'dist/').replace(/\.pug$/, '.html');
-    const srcPath = upath.resolve(upath.dirname(__filename), '../src');
-    const lang = "en";
 
-    console.log(`### INFO: Rendering ${filePath} to ${destPath}`);
-    const html = pug.renderFile(filePath, {
-        doctype: 'html',
-        filename: filePath,
-        basedir: srcPath,
-        lang,
-        dir: lang === "fa" ? "rtl" : "ltr",
-        translations,
-        t: function (key) {
-            return get(translations, `${lang}.${key}`);
+    for (const lang of LANGS) {
+        const destPath = filePath.replace(/src\/pug\//, 'dist/').replace(/\.pug$/, `-${lang}.html`);
+        const srcPath = upath.resolve(upath.dirname(__filename), '../src');
+
+        console.log(`### INFO: Rendering ${filePath} to ${destPath}`);
+        const html = pug.renderFile(filePath, {
+            doctype: 'html',
+            filename: filePath,
+            basedir: srcPath,
+            translations,
+            lang,
+            dir: lang === "fa" ? "rtl" : "ltr",
+            t: function (key) {
+                return get(translations, `${lang}.${key}`);
+            }
+        });
+
+        const destPathDirname = upath.dirname(destPath);
+        if (!sh.test('-e', destPathDirname)) {
+            sh.mkdir('-p', destPathDirname);
         }
-    });
 
-    const destPathDirname = upath.dirname(destPath);
-    if (!sh.test('-e', destPathDirname)) {
-        sh.mkdir('-p', destPathDirname);
+        const prettified = prettier.format(html, {
+            printWidth: 1000,
+            tabWidth: 4,
+            singleQuote: true,
+            proseWrap: 'preserve',
+            endOfLine: 'lf',
+            parser: 'html',
+            htmlWhitespaceSensitivity: 'ignore'
+        });
+
+        fs.writeFileSync(destPath, prettified);
     }
 
-    const prettified = prettier.format(html, {
-        printWidth: 1000,
-        tabWidth: 4,
-        singleQuote: true,
-        proseWrap: 'preserve',
-        endOfLine: 'lf',
-        parser: 'html',
-        htmlWhitespaceSensitivity: 'ignore'
-    });
-
-    fs.writeFileSync(destPath, prettified);
 };
